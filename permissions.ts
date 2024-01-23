f8e64e783988621a50afe2de56249c342d713154
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-01-23 15:43:13 +0000

    Add permissions enum

---
+++ permissions.ts
@@ -0,0 +1,16 @@
+export enum Permissions {
+  OrgRolesManage    = "org-roles-manage",
+  OrgUsersManage    = "org-users-manage",
+  OrgSettingsManage = "org-settings-manage",
+
+  EduCoursesManage  = "edu-courses-manage",
+  EduClassesManage  = "edu-classes-manage",
+  EduStudentsManage = "edu-students-manage",
+  EduHomeworkManage = "edu-homework-manage",
+
+  FinReportsAccess  = "fin-reports-access",
+  FinPaymentsManage = "fin-payments-manage",
+
+  UsrEnrollInClass  = "usr-enrollment",
+  UsrHasHomework    = "usr-homework",
+}
